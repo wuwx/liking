977e721c6f1b36f3163ecf6f6f97908f45a7ecd1
wuwx <wuweixin@gmail.com>
2012-08-25 23:58:14 +0800

    auto load likes helper

--- lib/liking/engine.rb
+++ lib/liking/engine.rb
@@ -1,5 +1,12 @@
 module Liking
   class Engine < ::Rails::Engine
     isolate_namespace Liking
+    
+    initializer 'liking.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        helper Liking::LikesHelper
+      end
+    end
+    
   end
 end
