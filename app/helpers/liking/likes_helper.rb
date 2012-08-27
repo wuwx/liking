a4174d1aad9ef4c0fa67738999e1659b8e337c9c
wuweixin <wuweixin@gmail.com>
2012-08-27 14:57:13 +0800

    locales

--- app/helpers/liking/likes_helper.rb
+++ app/helpers/liking/likes_helper.rb
@@ -8,17 +8,17 @@ module Liking
       user_ids = Like.where(:resource_id => resource.id, :resource_type => resource.class).pluck(:user_id)
 
       if user_ids.empty?
-        link_to "<i class='icon-heart'></i> 喜欢".html_safe,
+        link_to "<i class='icon-heart'></i> #{t 'liking.like'}".html_safe,
                 liking.polymorphic_path(:like, resource.class.to_s.foreign_key => resource.id),
                   :method => :post,
                   :class => "btn btn-small"
       elsif current_user and user_ids.include?(current_user.id)
-        link_to "<i class='icon-heart icon-red'></i> #{user_ids.size}人喜欢".html_safe,
+        link_to "<i class='icon-heart icon-red'></i> #{user_ids.size} #{t 'liking.like'}".html_safe,
                 liking.polymorphic_path(:like, resource.class.to_s.foreign_key => resource.id),
                   :method => :delete,
                   :class => "btn btn-small"
       else
-        link_to "<i class='icon-heart'></i> #{user_ids.size}人喜欢".html_safe,
+        link_to "<i class='icon-heart'></i> #{user_ids.size} #{t 'liking.like'}".html_safe,
                 liking.polymorphic_path(:like, resource.class.to_s.foreign_key => resource.id),
                   :method => :post,
                   :class => "btn btn-small"
